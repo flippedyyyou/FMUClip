b0bddf439d53410fac602ad7c7f9b7047dca502c
Shenyu Zhang <zsy_lookout@outlook.com>
2026-03-03 15:36:04 +0800

    Modify scripts

--- finegrained/clip_unlearn_finegrained.py
+++ finegrained/clip_unlearn_finegrained.py
@@ -33,7 +33,7 @@ from finegrained.load_dataset import build_test_dataloaders
 from finegrained.params import build_parser as build_base_parser
 
 
-DEFAULT_MASK_DIR = "/home/shenruoyan/FMUClip/finegrained/mask"
+DEFAULT_MASK_DIR = "finegrained/mask"
 
 
 def _get_model_device(model: torch.nn.Module) -> torch.device:
